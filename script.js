c8887f70dcbaac47d5d3311b47754622d1ef5246
Devansh Kukreja <devansh.mmis@gmail.com>
2024-03-01 20:32:07 +0530

    drawing lines and trails , experimenting with trignometry

--- script.js
+++ script.js
@@ -26,14 +26,29 @@ class Particle{
         this.y = Math.floor(Math.random() * this.effect.height);
         this.speedX = Math.random() * 5 - 2.5;
         this.speedY = Math.random() * 5 - 2.5;
+        this.history = [{x : this.x , y:this.y}];
+        this.maxLength = Math.floor(Math.random() * 100+ 10);
+        this.angle = 0;
     }
 
     draw(context){
         context.fillRect(this.x , this.y , 10 ,10 );
+        context.beginPath();
+        context.moveTo(this.history[0].x , this.history[0].y);
+        for(let i =0 ; i< this.history.length ; i++){
+            context.lineTo(this.history[i].x , this.history[i].y);
+        }
+        context.stroke();
     }
     update(){
-        this.x += this.speedX;
-        this.y += this.speedY;
+        this.angle += 0.5;
+        this.x += this.speedX + Math.sin(this.angle) * 10;
+        this.y += this.speedY + Math.cos(this.angle) * 13;
+        this.history.push({x: this.x , y:this.y})
+        if( this.history.length  > this.maxLength){
+            this.history.shift(); // shift method remove one element from the beginning of the array
+        }
+
     }
 
 }
